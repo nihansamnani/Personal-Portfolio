c959478b200dfa4c8b8c5d6597cb1fac56c20328
Nihan Samnani <nihansamnani@gmail.com>
2024-05-11 11:24:32 +0530

    Second Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("For Optimal viewing experience: Desktop recommended.")
+  function CircleFollow(){
+    window.addEventListener("mousemove", function(details){
+        document.querySelector(".pointer").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
+    })
+}
+
+CircleFollow();
